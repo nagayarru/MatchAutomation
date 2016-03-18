0bffed0b95c6f27a23e8661dcadd82147c759cc8
nagayarru <naga4qa@gmail.com>
2016-03-17 21:33:58 -0500

    Added the test

---
+++ MatchAutomation/MatchAutomation/DashboardPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+namespace MatchAutomation
+{
+    public class DashboardPage
+    {
+        public static bool IsAt
+        {
+            get
+            {
+                var subscribebtn = Driver.Instance.FindElements(By.ClassName("btnSubscribe"));
+                
+                    return subscribebtn[0].Text == "Subscribe";
+                
+            
+            }
+        }
+    }
+}
